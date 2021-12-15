f79b7161c8fa078dfff1ac5c82b33b6cb8a653e7
Freeman Bird <freemanbird@gmail.com>
2021-12-15 16:45:07 -0700

    testing an error

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ app.get('/css', function(req, res) {
 });
 
 app.get('/resources/:name', function(req, res) {
+    console.log(req.params);
     res.sendFile(path.join(__dirname, `/resources/${req.params.name}`));
 });
 
