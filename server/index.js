f4118b40a6d0d62b390948deef89209ac0cd8e08
Freeman Bird <freemanbird@gmail.com>
2021-12-15 16:46:27 -0700

    reverted to older working version ctrl z

--- server/index.js
+++ server/index.js
@@ -18,9 +18,40 @@ app.get('/css', function(req, res) {
     res.sendFile(path.join(__dirname, '../styles.css'));
 });
 
-app.get('/resources/:name', function(req, res) {
-    console.log(req.params);
-    res.sendFile(path.join(__dirname, `/resources/${req.params.name}`));
+app.get('/resources/zig.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/zig.png'));
+});
+
+app.get('/resources/remix.mp3', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/remix.mp3'));
+});
+
+app.get('/resources/woman.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/woman.png'));
+});
+
+app.get('/resources/evil_king.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/evil_king.png'));
+});
+
+app.get('/resources/download.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/download.png'));
+});
+
+app.get('/resources/free_minecraft.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/free_minecraft.png'));
+});
+
+app.get('/resources/white_pharaoh.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/white_pharaoh.png'));
+});
+
+app.get('/resources/spaceballs_chapter_12.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/spaceballs_chapter_12.png'));
+});
+
+app.get('/resources/yair_davidiy.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/yair_davidiy.png'));
 });
 
 const port = process.env.PORT || 4005;
