6aa933b4159c9d2e31b3866fd0e966ad1db5cdec
Freeman Bird <freemanbird@gmail.com>
2021-12-15 15:12:46 -0700

    changed more links for resources

--- server/index.js
+++ server/index.js
@@ -15,6 +15,26 @@ app.get('/css', function(req, res) {
     res.sendFile(path.join(__dirname, '../styles.css'));
 });
 
+app.get('/zig', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/zig.png'));
+});
+
+app.get('/remix', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/remix.mp3'));
+});
+
+app.get('/woman', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/woman.png'));
+});
+
+app.get('/evil_king', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/evil_king.png'));
+});
+
+app.get('/download', function(req, res) {
+    res.sendFile(path.join(__dirname, '../resources/download.png'));
+});
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {console.log(`COMLINK ${port} ESTABLISHED. AWAITING ORDERS`)});
