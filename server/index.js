95271596ead188d5f283cd62569b9a09c874f4cf
Freeman Bird <freemanbird@gmail.com>
2021-12-15 16:04:53 -0700

    fiddling around with more srcs

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use('/js', express.static(path.join(__dirname, '../index.js')))
-app.use('/remix', express.static(path.join(__dirname, '../resources/remix.mp3')))
+app.use('/resources/remix.mp3', express.static(path.join(__dirname, '../resources/remix.mp3')))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
@@ -18,23 +18,23 @@ app.get('/css', function(req, res) {
     res.sendFile(path.join(__dirname, '../styles.css'));
 });
 
-app.get('/zig', function(req, res) {
+app.get('/resources/zig.png', function(req, res) {
     res.sendFile(path.join(__dirname, '../resources/zig.png'));
 });
 
-app.get('/remix', function(req, res) {
+app.get('/resources/remix.mp3', function(req, res) {
     res.sendFile(path.join(__dirname, '../resources/remix.mp3'));
 });
 
-app.get('/woman', function(req, res) {
+app.get('/resources/woman.png', function(req, res) {
     res.sendFile(path.join(__dirname, '../resources/woman.png'));
 });
 
-app.get('/evil_king', function(req, res) {
+app.get('/resources/evil_king.png', function(req, res) {
     res.sendFile(path.join(__dirname, '../resources/evil_king.png'));
 });
 
-app.get('/download', function(req, res) {
+app.get('/resources/download.png', function(req, res) {
     res.sendFile(path.join(__dirname, '../resources/download.png'));
 });
 
