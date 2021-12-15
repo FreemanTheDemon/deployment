9ff7b3c4344654421ee6b84b0bc465eb9ce286d7
Freeman Bird <freemanbird@gmail.com>
2021-12-15 16:39:24 -0700

    added lots of content, changed up get requests

--- server/index.js
+++ server/index.js
@@ -18,24 +18,8 @@ app.get('/css', function(req, res) {
     res.sendFile(path.join(__dirname, '../styles.css'));
 });
 
-app.get('/resources/zig.png', function(req, res) {
-    res.sendFile(path.join(__dirname, '../resources/zig.png'));
-});
-
-app.get('/resources/remix.mp3', function(req, res) {
-    res.sendFile(path.join(__dirname, '../resources/remix.mp3'));
-});
-
-app.get('/resources/woman.png', function(req, res) {
-    res.sendFile(path.join(__dirname, '../resources/woman.png'));
-});
-
-app.get('/resources/evil_king.png', function(req, res) {
-    res.sendFile(path.join(__dirname, '../resources/evil_king.png'));
-});
-
-app.get('/resources/download.png', function(req, res) {
-    res.sendFile(path.join(__dirname, '../resources/download.png'));
+app.get('/resources/:name', function(req, res) {
+    res.sendFile(path.join(__dirname, `/resources/${req.name}`));
 });
 
 const port = process.env.PORT || 4005;
